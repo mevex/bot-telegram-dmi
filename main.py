aebefe0c8e94a4b397763c031002e25f3d186f3f
root <root@localhost.localdomain>
2019-07-02 15:26:21 +0200

    Main functions written, implementation missing

--- main.py
+++ main.py
@@ -6,10 +6,24 @@ from telegram.ext import Updater, CommandHandler
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
+
+def search_professor(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Cerca un professore usando il suo cognome")
+
+
+def show_planner(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Orario del giorno")
+
+
 updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
+search_professor_handler = CommandHandler('cerca_professore', search_professor)
+show_planner_handler = CommandHandler('mostra_orario', show_planner)
+
 dp.add_handler(start_handler)
+dp.add_handler(search_professor_handler)
+dp.add_handler(show_planner_handler)
 
 updater.start_polling()
