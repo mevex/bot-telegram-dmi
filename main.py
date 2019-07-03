570436b427c2ee867a0b995605fd84bfb35dd3f7
Matteo Digiorgio <matteo_digiorgio@Matteos-MacBook.station>
2019-07-03 22:03:31 +0200

    Buttons menu implemented

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 #!/root/Documents/progetto_ingegneria/venv_ingegneria/bin/python3
 from config import TOKEN
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
-
-from bs4 import BeautifulSoup
+import telegram
 import requests
+import telebot
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
+from bs4 import BeautifulSoup
+from telegram import ReplyKeyboardMarkup
+from telebot import types
 
 
 def start(update, context):
     start_msg = 'Benvenuto. Questo bot ti permetterá di cercare i contatti dei' \
-        'professori che ti interessano e gli orari di lezione di oggi.'
-    update.message.reply_markdown(start_msg)
+        ' professori che ti interessano e gli orari di lezione di oggi.'
+    markup = types.ReplyKeyboardMarkup(
+        one_time_keyboard=True, resize_keyboard=True)
+    button_search = types.KeyboardButton('/cerca_professore', )
+    button_plan = types.KeyboardButton('/mostra_orario')
+    markup.row(button_search, button_plan)
+    chat_id = update.message.chat_id
+    tb.send_message(chat_id=chat_id, text=start_msg, reply_markup=markup)
 
 
 def ask_professor_name(update, context):
@@ -51,8 +59,6 @@ def cancel(update, context):
     return ConversationHandler.END
 
 
-updater = Updater(token=TOKEN, use_context=True)
-dp = updater.dispatcher
 search_cnv = ConversationHandler(
     entry_points=[CommandHandler('cerca_professore', ask_professor_name)],
 
@@ -63,6 +69,9 @@ search_cnv = ConversationHandler(
     fallbacks=[CommandHandler('cancel', cancel)]
 )
 
+updater = Updater(token=TOKEN, use_context=True)
+dp = updater.dispatcher
+tb = telebot.TeleBot(TOKEN)
 dp.add_handler(search_cnv)
 dp.add_handler(CommandHandler('start', start))
 dp.add_handler(CommandHandler('mostra_orario', show_planner))
